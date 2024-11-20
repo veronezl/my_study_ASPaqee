fabbd2e5c6719a57accc4d5c3f41289f17222248
lucasveronez <lucasveronez.dev@gmail.com>
2024-11-20 15:57:53 -0300

    feat: add app.MapGet e app.MapPost

--- MyNewApp/Program.cs
+++ MyNewApp/Program.cs
@@ -1,9 +1,27 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
-
-
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var todos = new List<Todo>();
+
+app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id) =>
+{
+    var targetTodo = todos.SingleOrDefault(t => id == t.Id);
+    return targetTodo is null
+        ? TypedResults.NotFound()
+        : TypedResults.Ok(targetTodo);
+});
+
+app.MapPost("/todos", (Todo task) =>
+{
+    todos.Add(task);
+    return TypedResults.Created("/todos/{id}", task);
+});
 
 app.Run();
+
+public record Todo( int Id, string Name, DateTime DueDate, bool IsCompleted);
+
